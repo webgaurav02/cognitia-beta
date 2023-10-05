ba367ce2d0a738a14cfaca84cfd79eb1692eab0f
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-10-06 00:06:54 +0530

    Second sector of Main page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import './App.css';
 import background from './background.png'
+import Timeline from './timeline.js'
 import EventCarousel from './eventCarousel';
 // colors 90A8ED FF90E8 FFC900 23A094 
 function App() {
   return (
     <>
-    <div className='absolute h-24 w-screen bg-[#1A1A1A]'></div>
-    <div className=' bg-[#101010] font-poppins w-screen h-screen text-white'> 
-      <div className=' px-36 grid grid-rows-7 grid-cols-5 h-screen '>
+    <div className='fixed h-24 w-screen bg-[#1A1A1A] border-b-2 border-stone-300'></div>
+    <div className='h-24'></div>
+    <div className=' bg-[#101010] font-poppins w-screen h-11/12 text-white'> 
+      <div className=' px-36 grid grid-rows-7 grid-cols-5 h-full'>
         <div className=' row-span-2 col-span-2 text-9xl self-end'>COGNITIA 2K23 FEST</div>
         <div className='flex justify-end content-center row-span-6 col-span-3 '>      <img className=' self-center w-5/6 h-4/6 object-cover rounded-2xl' src={background} alt="background" /></div>
         <div className=' row-span-2 col-span-2 text-3xl text-gray-400 self-center'>Welcome to Cognitia, NIT Meghalaya’s annual tech and cultural fest. Explore all events and get registered now! </div>
-        <div className=' row-span-2 col-span-2 '><button className='border-black rounded-lg bg-[#F8D57E] text-black h-20 w-72 text-2xl'>View Events</button></div>
+        <div className=' row-span-2 col-span-2 '><button className='border-black rounded-lg bg-[#F8D57E] text-black h-20 w-72 text-2xl hover:bg-white'>View Events</button></div>
       </div>
-      </div>
-      <EventCarousel/>
-      <div className="w-11/12 h-[0px] border-2 border-stone-300"></div>
-      </>
+      
+    </div>
+    <div className='flex justify-center'>
+      <div className="w-11/12 border-b-2 border-stone-300"></div>
+    </div>
+    
+    <Timeline />
+
+    <div className='flex justify-center'>
+      <div className="w-11/12 border-b-2 border-stone-300"></div>
+    </div>
+    </>
   );
 }
 
---
+++ src/timeline.js
@@ -0,0 +1,101 @@
+
+export default function Timeline(){
+    return(
+        <div className=" bg-[#101010] h-[768px] w-screen">
+            <div className=" mx-36 grid grid-rows-1 grid-cols-3 h-full font-poppins">
+                <div className="flex self-center col-span-1 text-white text-8xl font-bold"> 
+                    Here's a timeline of all the events
+                </div>
+                <div className="grid grid-rows-3 col-span-2"> 
+                    <div className="grid grid-rows-2 py-6 px-20 flex-col self-end justify-self-center h-40 w-11/12 bg-white rounded-3xl text-[#1A202C]">
+                        <div className="flex text-xl">
+                            <div className="self-center ">
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect y="0.5" width="16" height="16" rx="8" fill="#626262" fill-opacity="0.3"/>
+                                <circle cx="8" cy="8.5" r="4" fill="white"/>
+                                </svg>
+                            </div>
+                            <div className="self-center">Nov 2nd </div>
+                        </div>
+                        <div className="flex w-full h-full overflow-clip">
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                        </div>                                         
+                    </div>
+                    <div className="grid grid-rows-2 py-6 px-20 flex-col self-center justify-self-center h-40 w-11/12 bg-white rounded-3xl text-[#1A202C]">
+                        <div className="flex text-xl">
+                            <div className="self-center">
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect y="0.5" width="16" height="16" rx="8" fill="#626262" fill-opacity="0.3"/>
+                                <circle cx="8" cy="8.5" r="4" fill="white"/>
+                                </svg>
+                            </div>
+                            <div className="self-center">Nov 3rd </div>
+                        </div>
+                        <div className="flex w-full h-full overflow-clip">
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                        </div>                                         
+                    </div>
+                    <div className="grid grid-rows-2 py-6 px-20 flex-col self-start justify-self-center h-40 w-11/12 bg-white rounded-3xl text-[#1A202C]">
+                        <div className="flex text-xl">
+                            <div className="self-center">
+                                <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <rect y="0.5" width="16" height="16" rx="8" fill="#626262" fill-opacity="0.3"/>
+                                <circle cx="8" cy="8.5" r="4" fill="white"/>
+                                </svg>
+                            </div>
+                            <div className="self-center">Nov 4th </div>
+                        </div>
+                        <div className="flex w-full h-full overflow-clip">
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Hack the box</div>
+                                <div>Time-10:15</div>
+                            </div>
+                            <div className="w-1/4 border-r-2 border-[#486284] mr-10">
+                                <div className=" text-xl">Andha Biswas</div>
+                                <div>Time-11:15</div>
+                            </div>
+                        </div>                                         
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
