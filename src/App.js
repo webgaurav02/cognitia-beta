5e89bf6b6c9ce513f850c2396f41b211497c39cf
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-10-06 16:32:31 +0530

    Changed background picture

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import background from './background.png'
 import Timeline from './timeline.js'
 import PopularEvents from './pop.js'
 import Footer from './footer';
@@ -9,13 +8,14 @@ function App() {
     <>
     <div className='fixed h-24 w-screen bg-[#1A1A1A] border-b-2 border-stone-300 z-50'></div>
     <div className='h-24'></div>
-    <div className=' bg-[#101010] font-poppins w-screen h-11/12 text-white'> 
-      <div className=' px-36 grid grid-rows-7 grid-cols-5 h-full'>
+    <div id="bgPic" className='opacity-50'></div>
+    <div className='font-poppins w-screen h-[768px] text-white -translate-y-full absolute'> 
+      <div className=' px-36 grid grid-rows-7 grid-cols-5 h-full '>
         <div className=' row-span-2 col-span-2 text-9xl self-end'>COGNITIA 2K23 FEST</div>
-        <div className='flex justify-end content-center row-span-6 col-span-3 '>      <img className=' self-center w-5/6 h-4/6 object-cover rounded-2xl' src={background} alt="background" /></div>
+        <div className='flex justify-end content-center row-span-6 col-span-3 '> </div>
         <div className=' row-span-2 col-span-2 text-3xl text-gray-400 self-center'>Welcome to Cognitia, NIT Meghalaya’s annual tech and cultural fest. Explore all events and get registered now! </div>
         <div className=' row-span-2 col-span-2 '><button className='border-black rounded-lg bg-[#F8D57E] text-black h-20 w-72 text-2xl hover:bg-white'>View Events</button></div>
-      </div>
+    </div>
       
     </div>
     <div className='flex justify-center'>
--- src/footer.js
+++ src/footer.js
@@ -1,48 +1,33 @@
 export default function Footer(){
     return(
-        <div className="w-screen h-screen relative">
-            <div className="w-[328px] left-[193px] top-[386.82px] absolute text-gray-200 text-base font-semibold font-['Plus Jakarta Sans']">
-                ©2023 NIT-Meghalaya. All rights reserved
-            </div>
-            <div className="w-36 left-[1369px] top-[386px] absolute text-right text-gray-200 text-base font-semibold font-['Plus Jakarta Sans']">
-                Terms & Condition
-            </div>
-            <div className="w-36 left-[1165px] top-[386px] absolute text-right text-gray-200 text-base font-semibold font-['Plus Jakarta Sans']">
-                Privacy & Policy
-            </div>
-            <div className="w-[152px] h-[244px] left-[1361px] top-[70px] absolute">
-                <div className="w-[152px] h-[188px] left-0 top-[56px] absolute text-gray-200 text-base font-medium font-['Plus Jakarta Sans']">
-                    Discord<br/><br/>
-                    Instagram<br/><br/>
-                    Twitter<br/><br/>Facebook
+        <div className="px-64 w-screen h-[40vh] text-white font-poppins">
+            <div className="grid grid-cols-3 w-full h-full">
+                <div className="flex-col space-y-3 self-center grid-cols-3 w-4/5 ">
+                    <div className=" font-poppins text-2xl">COGNITIA</div>
+                    <div className=" font-poppins">The official NIT Meghalaya Tech and cultural
+                    landing page and registrration portal</div>
+                    <div className="flex space-x-2">
+                        <div>
+                            <svg className=" fill-white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 25 3 C 12.861562 3 3 12.861562 3 25 C 3 36.019135 11.127533 45.138355 21.712891 46.728516 L 22.861328 46.902344 L 22.861328 29.566406 L 17.664062 29.566406 L 17.664062 26.046875 L 22.861328 26.046875 L 22.861328 21.373047 C 22.861328 18.494965 23.551973 16.599417 24.695312 15.410156 C 25.838652 14.220896 27.528004 13.621094 29.878906 13.621094 C 31.758714 13.621094 32.490022 13.734993 33.185547 13.820312 L 33.185547 16.701172 L 30.738281 16.701172 C 29.349697 16.701172 28.210449 17.475903 27.619141 18.507812 C 27.027832 19.539724 26.84375 20.771816 26.84375 22.027344 L 26.84375 26.044922 L 32.966797 26.044922 L 32.421875 29.564453 L 26.84375 29.564453 L 26.84375 46.929688 L 27.978516 46.775391 C 38.71434 45.319366 47 36.126845 47 25 C 47 12.861562 37.138438 3 25 3 z M 25 5 C 36.057562 5 45 13.942438 45 25 C 45 34.729791 38.035799 42.731796 28.84375 44.533203 L 28.84375 31.564453 L 34.136719 31.564453 L 35.298828 24.044922 L 28.84375 24.044922 L 28.84375 22.027344 C 28.84375 20.989871 29.033574 20.060293 29.353516 19.501953 C 29.673457 18.943614 29.981865 18.701172 30.738281 18.701172 L 35.185547 18.701172 L 35.185547 12.009766 L 34.318359 11.892578 C 33.718567 11.811418 32.349197 11.621094 29.878906 11.621094 C 27.175808 11.621094 24.855567 12.357448 23.253906 14.023438 C 21.652246 15.689426 20.861328 18.170128 20.861328 21.373047 L 20.861328 24.046875 L 15.664062 24.046875 L 15.664062 31.566406 L 20.861328 31.566406 L 20.861328 44.470703 C 11.816995 42.554813 5 34.624447 5 25 C 5 13.942438 13.942438 5 25 5 z"/></svg>
+                        </div>
+                        <div>
+                            <svg className=" fill-white" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 16 3 C 8.8324839 3 3 8.8324839 3 16 L 3 34 C 3 41.167516 8.8324839 47 16 47 L 34 47 C 41.167516 47 47 41.167516 47 34 L 47 16 C 47 8.8324839 41.167516 3 34 3 L 16 3 z M 16 5 L 34 5 C 40.086484 5 45 9.9135161 45 16 L 45 34 C 45 40.086484 40.086484 45 34 45 L 16 45 C 9.9135161 45 5 40.086484 5 34 L 5 16 C 5 9.9135161 9.9135161 5 16 5 z M 37 11 A 2 2 0 0 0 35 13 A 2 2 0 0 0 37 15 A 2 2 0 0 0 39 13 A 2 2 0 0 0 37 11 z M 25 14 C 18.936712 14 14 18.936712 14 25 C 14 31.063288 18.936712 36 25 36 C 31.063288 36 36 31.063288 36 25 C 36 18.936712 31.063288 14 25 14 z M 25 16 C 29.982407 16 34 20.017593 34 25 C 34 29.982407 29.982407 34 25 34 C 20.017593 34 16 29.982407 16 25 C 16 20.017593 20.017593 16 25 16 z"/></svg>
+                        </div>
+                    </div>
                 </div>
-            <div className="w-[152px] h-8 left-0 top-0 absolute text-gray-200 text-xl font-semibold font-['Plus Jakarta Sans']">Socials</div>
-        </div>
-        <div className="w-[152px] h-[244px] left-[1058.14px] top-[70px] absolute">
-            <div className="w-[152px] h-[188px] left-0 top-[56px] absolute text-gray-200 text-base font-medium font-['Plus Jakarta Sans']">
-                Events<br/><br/>
-                Blog<br/><br/>
-                Podcast<br/><br/>
-                Invite a friend
+                <div className="flex-col space-y-5 justify-self-center self-center grid-cols-3 ">
+                    <div className=" font-poppins text-xl">Links</div>
+                    <div>Home</div>
+                    <div>Events</div>
+                    <div>Team</div>
+                </div>
+                <div className="flex-col space-y-5 justify-self-end self-center grid-cols-3">
+                    <div className=" font-poppins text-xl">Socials</div>
+                    <div>Instagram</div>
+                    <div>Twitter</div>
+                    <div>Facebook</div>
+                </div>
             </div>
-        <div className="w-[152px] h-8 left-0 top-0 absolute text-gray-200 text-xl font-semibold font-['Plus Jakarta Sans']">Community</div>
-        </div>
-        <div className="w-[152px] h-[244px] left-[755.29px] top-[70px] absolute">
-            <div className="w-[152px] h-[188px] left-0 top-[56px] absolute text-gray-200 text-base font-medium font-['Plus Jakarta Sans']">
-                How it works<br/><br/>
-                Featured<br/><br/>
-                Partnership<br/><br/>
-                Bussiness Relation
-            </div>
-            <div className="w-[152px] h-8 left-0 top-0 absolute text-gray-200 text-xl font-semibold font-['Plus Jakarta Sans']]">About</div>
-        </div>
-        <div className="w-[1320px] h-[0px] left-[1513px] top-[350px] absolute origin-top-left -rotate-180 border border-slate-600 border-opacity-20"></div>
-        <div className="left-[193px] top-[150.86px] absolute flex-col justify-start items-start gap-4 inline-flex">
-            <div className="w-[318px] text-gray-200 text-base font-medium font-['Plus Jakarta Sans'] ">
-                The official NIT Meghalaya Tech and Cultural fest Landing page and registration portal
-            </div>
-        </div>
-        <div className="w-[174.16px] h-11 left-[193px] top-[86px] absolute text-gray-200 text-[32px] font-semibold font-['Plus Jakarta Sans']">COGNITIA</div>
         </div>
     );
 }
