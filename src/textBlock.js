c72ab63514ec7eecbf6f6e0bbe18be8b650fa2a5
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-10-07 14:21:30 +0530

    Completed Parallax Effect

---
+++ src/textBlock.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function TextBlock() {
+    return (
+        <div id="textblock" className=' h-[350vh] sm:translate-y-[50vh] md:translate-y-[75vh] lg:translate-y-[128vh] xl:translate-y-[190vh] 2xl:h-0'>
+            <div id="textblock-container">
+            </div>
+        </div>
+    );
+}
+
+export default TextBlock;
--- src/timeline.js
+++ src/timeline.js
@@ -2,14 +2,22 @@
 export default function Timeline(){
     return(
         <div className=" bg-[rgb(16,16,16)] h-[768px] w-screen">
-            <div className=" mx-3 mt-10 md:0 md:mx-36 md:grid grid-rows-1 grid-cols-3 h-full font-poppins">
-                <div className="flex px-5 translate-y-10 md:px-0 md:translate-y-0 self-center col-span-1 text-white text-3xl md:text-8xl font-poppinsbold"> 
+            <div className=" mx-3 mt-10 md:0 md:mx-36 md:grid 
+            grid-rows-1 grid-cols-3 h-full 
+            font-poppins">
+                <div className="flex self-center col-span-1
+                px-5 translate-y-10 md:px-0 md:translate-y-0  
+                text-white text-3xl md:text-4xl lg:text-6xl xl:text-7xl 2xl:text-8xl font-poppinsbold"> 
                     Here's a timeline of all the events
                 </div>
-                <div className="grid grid-rows-3 col-span-2 text-xs md:text-xl h-3/4 w-full md:h-full md:w-full"> 
-                    <div className="grid grid-rows-2 py-2 px-2 md:py-6 md:px-20 flex-col self-end justify-self-center h-24 md:h-40 md:w-11/12 bg-white 
-                      hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4
-                        rounded-lg md:rounded-3xl text-[#1A202C] duration-300">
+                <div className="grid grid-rows-3 col-span-2 
+                h-3/4 w-full md:h-full md:w-full
+                text-xs 2xl:text-xl"> 
+                    <div className="grid grid-rows-2 self-end justify-self-center
+                    py-2 px-2 md:py-6 md:px-10 2xl:px-20 h-20 lg:h-32 md:h-28 xl:h-32 2xl:h-40 md:w-11/12 
+                    bg-white rounded-lg md:rounded-3xl
+                    hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4 duration-300
+                    text-[#1A202C]">
                         <div className="flex md:text-xl">
                             <div className="self-center ">
                                 <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -21,26 +29,28 @@ export default function Timeline(){
                         </div>
                         <div className="flex w-full h-full overflow-clip">
                             <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
                         </div>                                         
                     </div>
-                    <div className="grid grid-rows-2 py-2 px-2 md:py-6 md:px-20 flex-col self-center justify-self-center h-24  md:h-40 md:w-11/12 bg-white 
-                    hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4
-                    rounded-lg md:rounded-3xl text-[#1A202C] duration-300">
+                    <div className="grid grid-rows-2 self-center justify-self-center
+                    py-2 px-2 md:py-6 md:px-10 2xl:px-20 lg:h-32 h-20 md:h-28 xl:h-32 2xl:h-40 md:w-11/12 
+                    bg-white rounded-lg md:rounded-3xl
+                    hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4 duration-300
+                    text-[#1A202C]">
                         <div className="flex md:text-xl">
                             <div className="self-center ">
                                 <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -51,27 +61,29 @@ export default function Timeline(){
                             <div className="self-center">Nov 2nd </div>
                         </div>
                         <div className="flex w-full h-full overflow-clip">
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284]  md:mr-10">
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
                         </div>                                         
                     </div>
-                    <div className="grid grid-rows-2 py-2 px-2 md:py-6 md:px-20 flex-col self-start justify-self-center h-24 md:h-40 md:w-11/12 bg-white 
-                    hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4
-                    rounded-lg md:rounded-3xl text-[#1A202C] duration-300">
+                    <div className="grid grid-rows-2 self-start justify-self-center
+                    py-2 px-2 md:py-6 md:px-10 2xl:px-20 h-20 lg:h-32 md:h-28 xl:h-32 2xl:h-40 md:w-11/12 
+                    bg-white rounded-lg md:rounded-3xl
+                    hover:bg-black hover:text-white hover:border-stone-300 hover:border-2 hover:shadow-white shadow-lg hover:translate-x-4 duration-300
+                    text-[#1A202C]">
                         <div className="flex md:text-xl">
                             <div className="self-center ">
                                 <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -83,20 +95,20 @@ export default function Timeline(){
                         </div>
                         <div className="flex w-full h-full overflow-clip">
                             <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Hack the box</div>
-                                <div>Time-10:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Hack the box</div>
+                                <div className=" invisible xl:visible">Time-10:15</div>
                             </div>
-                            <div className="md:w-1/4 border-r-2 border-[#486284] md:mr-10">
-                                <div className=" md:text-xl font-poppinsbold">Andha Biswas</div>
-                                <div>Time-11:15</div>
+                            <div className="md:w-1/4 border-r-2 border-[#486284] pl-2 md:pl-0 md:mr-10">
+                                <div className=" font-poppinsbold">Andha Biswas</div>
+                                <div className=" invisible xl:visible">Time-11:15</div>
                             </div>
                         </div>                                         
                     </div>
