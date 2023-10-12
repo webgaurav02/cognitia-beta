61b50b3f394367159e7077251cc3f3ce2a3a87c9
Samuel Khongthaw <skthaw1@gmail.com>
2023-10-12 17:46:49 +0530

    highlight nav links

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,7 +1,9 @@
+"use client";
 import Link from "next/link";
 import Logo from "../assets/logo.png";
 import Image from "next/image";
 import { jakarta } from "../utils/fonts";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
   function buildLine() {
@@ -9,6 +11,7 @@ export default function NavBar() {
       <hr class=" duration-300 w-[100%] t-4 h-[1px] mx-auto bg-gray-100 border-0 rounded dark:bg-[#9A9A9A]"></hr>
     );
   }
+  const pathName = usePathname();
   return (
     <>
       <div class="w-full z-50 h-24 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-40 sticky top-0 bg-[#101010] pr-6 pl-6">
@@ -21,13 +24,31 @@ export default function NavBar() {
 
           <div className="flex flex-row self-center text-neutral-200 font-medium space-x-6 md:text-lg text-sm">
             <Link className={jakarta.className} href="/">
-              <div className="cursor-pointer">Home</div>
+              <div
+                className={`cursor-pointer ${
+                  pathName === "/" ? "border-b-2 pb-1" : ""
+                }`}
+              >
+                Home
+              </div>
             </Link>
             <Link className={jakarta.className} href={"/events"}>
-              <div className="cursor-pointer">Events</div>
+              <div
+                className={`cursor-pointer ${
+                  pathName === "/events" ? "border-b-2 pb-1" : ""
+                }`}
+              >
+                Events
+              </div>
             </Link>
             <Link className={jakarta.className} href={"/team"}>
-              <div className="cursor-pointer">Teams</div>
+              <div
+                className={`cursor-pointer ${
+                  pathName === "/team" ? "border-b-2 pb-1" : ""
+                }`}
+              >
+                Teams
+              </div>
             </Link>
           </div>
         </div>
