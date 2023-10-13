136fa6a3931f47d654639f60fac09f8463102bcd
Samuel Khongthaw <skthaw1@gmail.com>
2023-10-13 11:20:10 +0530

    redesigned nav and home banner

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -14,15 +14,15 @@ export default function NavBar() {
   const pathName = usePathname();
   return (
     <>
-      <div class="w-full z-50 h-24 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-40 sticky top-0 bg-[#101010] pr-6 pl-6">
+      <div class="w-full z-50 h-20 backdrop-filter backdrop-blur-md border-b border-gray-200 bg-opacity-40 sticky top-0 bg-[#101010] pr-6 pl-6">
         <div className="h-full flex flex-row justify-between content-center md:mr-32 md:ml-32">
           <div className="text-neutral-200 md:text-[30px] self-center font-medium text-2xl">
             <Link href={"/"}>
-              <Image width={80} height={80} src={Logo} alt="logo" />
+              <Image width={60} height={60} src={Logo} alt="logo" />
             </Link>
           </div>
 
-          <div className="flex flex-row self-center text-neutral-200 font-medium space-x-6 md:text-lg text-sm">
+          <div className="flex flex-row self-center text-neutral-200 font-medium space-x-6 md:text-lg text-xs">
             <Link className={jakarta.className} href="/">
               <div
                 className={`cursor-pointer ${
@@ -35,7 +35,7 @@ export default function NavBar() {
             <Link className={jakarta.className} href={"/events"}>
               <div
                 className={`cursor-pointer ${
-                  pathName === "/events" ? "border-b-2 pb-1" : ""
+                  pathName === "/events" ? "border-b-2 pb-1 " : ""
                 }`}
               >
                 Events
