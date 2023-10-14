722c442ff8b40b4d08f58b81a77efe8f68cfa261
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-10-14 14:28:05 +0530

    Fixed weird bug where button link distance was extended

--- app/components/parallax-home.js
+++ app/components/parallax-home.js
@@ -142,13 +142,15 @@ function ParallaxHome() {
                   </div>
                 </div>
                 <div className="pl-4 mt-2 py-2 sm:py-0 row-span-1 col-span-3">
-                  <Link href="/events">
+
                     <div className={jost.className}>
+                    <Link href="/events">
                       <button className="border-[#E1ABE8] border-[1px] lg:mt-10 sm:border-2 rounded-lg text-[#E1ABE8] hover:text-black text-lg font-medium h-10 w-32 sm:h-20 sm:w-72 sm:text-2xl hover:bg-[#E1ABE8] duration-500">
                         View Events
                       </button>
+                      </Link>
                     </div>
-                  </Link>
+
                 </div>
               </div>
             </div>
