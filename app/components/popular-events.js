b9769a1a37cab07d528140abe96ef558de1476e7
Isaiah Khonglah <isaiahlemiker@gmail.com>
2023-10-14 17:57:48 +0530

    Fixed popular events page from overflowing upwards

--- app/components/popular-events.js
+++ app/components/popular-events.js
@@ -3,7 +3,7 @@ export default function PopularEvents() {
   return (
     <div className="h-screen w-screen">
       <div className="mt-20 sm:mt-0 sm:grid sm:grid-rows-5 sm:grid-cols-7 h-full font-poppins">
-        <div className=" mx-10 sm:mx-36 sm:row-span-3 sm:col-span-5 text-white text-3xl sm:text-4xl md:text-5xl lg:text-7xl self-center font-poppinsbold">
+        <div className=" mx-10 sm:mx-36 sm:row-span-3 sm:col-span-5 text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl self-center font-poppinsbold">
           Psst...
           <br /> check out the most popular events
         </div>
