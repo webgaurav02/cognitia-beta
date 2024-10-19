17d9dbd090220d703dcafae556750c779ab63708
Jishnu Duhan <duhanjishnu@gmail.com>
2024-10-19 21:36:53 +0530

    Add dev and design

--- app/team/page.js
+++ app/team/page.js
@@ -2,17 +2,16 @@
 
 import "./team.css";
 import React, { useState } from "react";
-import image0 from "../assets/images/download 8.jpg"; 
+import image0 from "../assets/images/download 8.png"; 
 //devteam
-import image1 from "../assets/images/dev/sam.jpg";
-import image2 from "../assets/images/dev/Kilio.jpg";
-import image3 from "../assets/images/dev/Amal Shibu.jpg";
-import image4 from "../assets/images/dev/Isaiah.jpeg";
-import image5 from "../assets/images/dev/Nishant.jpeg";
-import image6 from "../assets/images/dev/Marjiba.jpeg";
-import image8 from "../assets/images/dev/Gunnu Lavanya.jpg";
-import image7 from "../assets/images/dev/Gaurav Polai.jpg";
-import gaurav from "@/app/assets/images/dev/Gaurav.jpeg"
+import image1 from "../assets/images/dev/Gaurav.jpeg";
+import image2 from "../assets/images/dev/jishnu.jpg";
+import image3 from "../assets/images/dev/charity.jpg";
+// import image4 from "../assets/images/dev/Isaiah.jpeg";
+// import image5 from "../assets/images/dev/Nishant.jpeg";
+// import image6 from "../assets/images/dev/Marjiba.jpeg";
+// import image8 from "../assets/images/dev/Gunnu Lavanya.jpg";
+// import image7 from "../assets/images/dev/Gaurav Polai.jpg";
 //sac committee
 
 import image9 from "../assets/images/SAC Committee/Atanu.jpg";
@@ -29,14 +28,14 @@ import image17 from "../assets/images/Core/supriya.jpg";
 import image18 from "../assets/images/Core/OM PRAKASH YADAV.jpg";
 
 //Design
-import image20 from "../assets/images/design/vadan.jpg";
-import image21 from "../assets/images/design/L Komune.jpg";
-import image22 from "../assets/images/design/Jaswanth Bojja.jpg";
-import image23 from "../assets/images/design/Dhanunjay Naidu Paruvada.jpg";
-import image24 from "../assets/images/design/SUBHADEEP SINHA.jpg";
+import image20 from "../assets/images/design/paruvada Dhanunjay Naidu.png";
+import image21 from "../assets/images/design/SUBHADEEP SINHA.jpg";
+import image22 from "../assets/images/design/Botu Varun Kumar.jpg";
+import image23 from "../assets/images/design/M Manikanta Reddy.jpg";
+import image24 from "../assets/images/design/Karipireddy Surya Teja Gopal Reddy.jpg";
 import image25 from "../assets/images/design/Pankaj Saraswat.jpg";
-import image26 from "../assets/images/design/Deinipaya Alvira Lynshing.png";
-import image27 from "../assets/images/design/Botu Varun Kumar.jpg";
+// import image26 from "../assets/images/design/Deinipaya Alvira Lynshing.png";
+// import image27 from "../assets/images/design/Botu Varun Kumar.jpg";
 //import image28 from "../assets/images/design/
 
 //Marketing
@@ -127,35 +126,34 @@ const TeamSection = ({ images, names, roles }) => {
 function Devteam() {
   const images = [
     image1,
-    gaurav,
     image2,
     image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
+    // image4,
+    // image5,
+    // image6,
+    // image7,
+    // image8,
   ];
   const names = [
-    "SAM",
     "Gaurav",
-    "Kilio",
-    "Amal",
-    "Isaiah",
-    "Nishant",
-    "Marjiba",
-    "Gaurav",
-    "Lavanya",
+    "Jishnu",
+    "Charity",
+    // "Amal",
+    // "Isaiah",
+    // "Nishant",
+    // "Marjiba",
+    // "Gaurav",
+    // "Lavanya",
   ];
   const roles = [
     "Lead developer",
     "Lead developer",
     "Lead developer",
-    "Lead developer",
-    "Lead developer",
-    "Lead designer",
-    "Apprentice",
-    "Apprentice",
+    // "Lead developer",
+    // "Lead developer",
+    // "Lead designer",
+    // "Apprentice",
+    // "Apprentice",
   ];
 
   return <TeamSection images={images} names={names} roles={roles} />;
@@ -202,27 +200,19 @@ function Core() {
 function Design() {
   const images = [
     image20,
-    image1,
     image21,
     image22,
     image23,
     image24,
     image25,
-    image26,
-    image27,
-    image0,
   ];
   const names = [
-    "Sri Vadan",
-    "Samuel",
-    "L Komune",
-    "Jaswanth",
-    "Dhanunjay Naidu",
-    "SUBHADEEP SINHA",
-    "Pankaj Saraswat",
-    "Deinipaya Alvira",
-    "Varun B",
-    "Binesh Das",
+    "P Dhanunjay",
+    "Shubhadeep Sinha",
+    "Botu Varun",
+    "M.Manikanta Reddy",
+    "K. Surya Teja Gopal Reddy ",
+    "Pankaj Tripati",
   ];
   const roles = [
     "Convener",
