189f68d2562a5d47945b7bd8f29e3c69351b5cfa
webgaurav02 <gauravcodes123@gmail.com>
2024-10-20 23:08:37 +0530

    Merged charity into main, keeping all changes from charity in teams page

--- app/team/page.js
+++ app/team/page.js
@@ -1,14 +1,9 @@
 "use client"
 
-<<<<<<< HEAD
-import "./team.css";
-import React, { useState,useEffect} from "react";
-import image0 from "../assets/images/download 8.jpg"; 
-=======
+
 import "./team.css"
 import React, { useState } from "react"
 import image0 from "../assets/images/download 8.png"
->>>>>>> origin/charity
 //devteam
 import image1 from "../assets/images/dev/Gaurav.jpeg"
 import image2 from "../assets/images/dev/jishnu.jpg"
@@ -20,24 +15,6 @@ import image3 from "../assets/images/dev/charity.jpg"
 // import image7 from "../assets/images/dev/Gaurav Polai.jpg";
 //sac committee
 
-<<<<<<< HEAD
-import image9 from "../assets/images/SAC Committee/Atanu.jpg";
-import image10 from "../assets/images/SAC Committee/bunil.jpg";
-import image11 from "../assets/images/SAC Committee/swarop.jpg";
-import image12 from "../assets/images/Core/OM PRAKASH YADAV.jpg";
-import image13 from "../assets/images/SAC Committee/Arush.jpg";
-import image13A from "../assets/images/SAC Committee/Indra.jpg";
-
-
-//CORE
-// import image14 from "../assets/images/Core/Prakhar Gupta.jpg";
-import image15 from "../assets/images/Core/Gannu Abhijith Sai.jpg";
-import image16 from "../assets/images/Core/TVikram Rathod.jpg";
-import image17 from "../assets/images/Core/Neelkantha Mandal.jpg";
-import image18 from "../assets/images/Core/Jitta Chandra Sekhar.jpg";
-
-
-=======
 import image9 from "../assets/images/SAC Committee/Atanu.jpg"
 import image10 from "../assets/images/SAC Committee/bunil.jpg"
 import image11 from "../assets/images/SAC Committee/pardhu.png"
@@ -50,7 +27,6 @@ import image15 from "../assets/images/Core/sameer.jpg"
 import image16 from "../assets/images/Core/WanrapbokRKharshandi.jpg"
 import image17 from "../assets/images/Core/supriya.jpg"
 import image18 from "../assets/images/Core/OM PRAKASH YADAV.jpg"
->>>>>>> origin/charity
 
 //Design
 import image20 from "../assets/images/design/paruvada Dhanunjay Naidu.png"
@@ -77,37 +53,6 @@ import image40 from "../assets/images/marketing/Kabesa S Marak.png"
 import image41 from "../assets/images/marketing/Neelkantha Mandal.jpg"
 
 //Management
-<<<<<<< HEAD
-import image51 from "../assets/images/management/20240323131237_IMG_6123 - Manish Kumar Bishnoi.jpg";
-import image52 from "../assets/images/management/ig_sd - Sojib Datta.jpg";
-import image53 from "../assets/images/management/IMG_20240703_003842_033 - Mushfique Ahmed.jpg";
-import image54 from "../assets/images/management/IMG_20241011_021026 - Minaxi Kumari.jpg";
-import image55 from "../assets/images/management/IMG_20241011_104705 - Yashvi Arya.jpg";
-import image56 from "../assets/images/management/IMG_20241011_182243 - Podugu Ashrith.jpg";
-import image57 from "../assets/images/management/Photo - Nazia Hasan Sheikh (1).jpg";
-import image58 from "../assets/images/management/Screenshot_20241011-143337~2 - Syed Mohammad Ashraf Uddin Rafi.png";
-import image59 from "../assets/images/management/Vasundhara - Vasundhara Singh.jpeg";
-
-
-//others
-
-import image71 from "../assets/images/others/Laxman.jpg";
-import image72 from "../assets/images/others/Vedant.jpg";
-import image73 from "../assets/images/others/Satyajeet.jpg";
-import image74 from "../assets/images/others/Vattimilli Divya Sree.jpg";
-import image75 from "../assets/images/others/Gowthaboina Trisha.jpg";
-import image76 from "../assets/images/others/Sravani.webp";
-import image77 from "../assets/images/others/sanjana thothu.jpg";
-import image78 from "../assets/images/others/Rai  Tripura.jpeg";
-import image79 from "../assets/images/others/Rounak Saha.jpg";
-import image80 from "../assets/images/others/Arunoday Tiwari.jpg";
-import image81 from "../assets/images/others/Hemchand.jpg";
-import image82 from "../assets/images/others/Rama Krishna.jpg";
-import image83 from "../assets/images/others/MD SHAMS TABREZ ANSARI.jpg";
-import image84 from "../assets/images/others/Dharam Singh.jpg";
-import image85 from "../assets/images/others/Priyanshu Singh.jpg";
-import image86 from "../assets/images/others/Anil Kumar.jpg";
-=======
 import image51 from "../assets/images/management/Arilam Kyndiah.jpg"
 import image52 from "../assets/images/management/hrangaolou.jpg"
 import image53 from "../assets/images/management/Koninika Tarafdar.jpg"
@@ -139,7 +84,6 @@ import image77 from "../assets/images/others/Karimi Tarun Teja.jpeg"
 import image78 from "../assets/images/others/L SRI KASYAP.jpg"
 import image79 from "../assets/images/others/Simma kiran.jpg"
 //import image80 from "../assets/images/others/
->>>>>>> origin/charity
 
 
 // robotics
@@ -218,29 +162,6 @@ import { algreya, poppins } from "../utils/fonts"
 import Footer from "../components/footer"
 
 const TeamSection = ({ images, names, roles }) => {
-<<<<<<< HEAD
-  return (
-    <div className="w-full grid grid-cols-2 sm:grid-cols-4 mobile:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-6 gap-4 justify-between  bg-[#101010] mx-5 my-10 lg:my-20 lg:mx-10 font-poppins">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className="border-solid text-center bg-[#1C1C1C] w-75% sm:w-100% md:w-100% mb-5 mx-2 lg:mb-10  rounded-lg"
-        >
-          <Image
-            width={1000}
-            height={1000}
-            src={image}
-            alt={`Image ${index + 1}`}
-            className="object-cover max-w-full rounded-t-lg aspect-[3/4]"
-            placeholder="blur"
-          />
-          <div
-            className={`text-white mt font-poppins font-bold text-xl flex flex-col ${
-              image === image10 ? "text-[19px]" : ""
-            }`}
-          >
-            <strong className={poppins.className}>{names[index]}</strong>
-=======
 	return (
 		<div className="w-full grid grid-cols-2 sm:grid-cols-4 mobile:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-6 gap-4 justify-between  bg-[#101010] mx-5 my-10 lg:my-20 lg:mx-10 font-poppins">
 			{images.map((image, index) => (
@@ -262,7 +183,6 @@ const TeamSection = ({ images, names, roles }) => {
 						}`}
 					>
 						<strong className={poppins.className}>{names[index]}</strong>
->>>>>>> origin/charity
 
 						<span className={algreya.className}>
 							{" "}
@@ -276,40 +196,6 @@ const TeamSection = ({ images, names, roles }) => {
 }
 
 function Devteam() {
-<<<<<<< HEAD
-  const images = [
-    image1,
-    gaurav,
-    image2,
-    image3,
-    image4,
-    image5,
-    image6,
-    image7,
-    image8,
-  ];
-  const names = [
-    "SAM",
-    "Gaurav",
-    "Kilio",
-    "Amal",
-    "Isaiah",
-    "Nishant",
-    "Marjiba",
-    "Gaurav",
-    "Lavanya",
-  ];
-  const roles = [
-    "Lead developer",
-    "Lead developer",
-    "Lead developer",
-    "Lead developer",
-    "Lead developer",
-    "Lead designer",
-    "Apprentice",
-    "Apprentice",
-  ];
-=======
 	const images = [
 		image1,
 		image2,
@@ -341,31 +227,10 @@ function Devteam() {
 		// "Apprentice",
 		// "Apprentice",
 	]
->>>>>>> origin/charity
-
 	return <TeamSection images={images} names={names} roles={roles} />
 }
 
 function SAC() {
-<<<<<<< HEAD
-  const images = [image9, image10,image11,image12, image13,image13A];
-  const names = [
-    "Dr. Atanu Singha Roy",                  
-    "Dr. Bunil Kr. Balabantaray",
-    "T.Swaroop Kumar",
-    "Om Prakash Yadav",
-    "Arush Raman",
-    "IndraShikhar Sharma",
-  ];
-  const roles = [
-    "SAC President",
-    "Vice-President, Technical",
-    "General Secretary 1,Science & Technology",
-    "General Secretary 2,Science & Technology",
-    "Member 1,Science & Technology",
-    "Member 2,Science & Technology",
-  ];
-=======
 	const images = [image9, image10, image11, image12, image13]
 	const names = [
 		"Dr. Atanu Singha Roy",
@@ -381,28 +246,10 @@ function SAC() {
 		"General Secretary 2,Science & Technology",
 		"Member 1,Science & Technology",
 	]
->>>>>>> origin/charity
 
 	return <TeamSection images={images} names={names} roles={roles} />
 }
 function Core() {
-<<<<<<< HEAD
-  const images = [image0, image15, image16, image17, image18];
-  const names = [
-    "Ganesh Baidi",
-    "Gannu Abhijith Sai",
-    "T Vikram Rathod",
-    "Neelkantha Mandal",
-    "Jitta Chandra Sekhar",
-  ];
-  const roles = [
-    "Coordinator",
-    "Coordinator",
-    "Co-coordinator",
-    "Co-coordinator",
-    "Co-coordinator",
-  ];
-=======
 	const images = [image14, image15, image16, image17, image18]
 	const names = [
 		"Prakhar Gupta",
@@ -418,7 +265,6 @@ function Core() {
 		"Co-coordinator",
 		"Co-coordinator",
 	]
->>>>>>> origin/charity
 
 	return <TeamSection images={images} names={names} roles={roles} />
 }
@@ -491,61 +337,6 @@ function Marketing() {
 	return <TeamSection images={images} names={names} roles={roles} />
 }
 function Management() {
-<<<<<<< HEAD
-  const images = [
-    image51,
-    image52,
-    image53,
-    image54,
-    image55,
-    image56,
-    image57,
-    image58,
-    image59,
-    image0,
-    image0,
-    image0,
-    image0,
-    image0,
-  ];
-  const names = [
-    "Manish Kumar Bishnoi",
-    "Sojib Datta",
-    "Mushfique Ahmed",
-    "Minaxi Kumari",
-    "Yashvi Arya",
-    "Podugu Ashrith",
-    "Nazia Hasan Sheik",
-    "Syed Mohammad Ashraf Uddin Rafi",
-    "Vasundhara Singh",
-    "Penugurthi Dinesh",
-    "B Palguna Rao",
-    "Abhishek",
-    "Rakesh Thumu",
-    "Rahul Kumar",
-    // "B Sai Upender",
-    // "Olivia El Reka",
-    // "Penugurthi Dinesh",
-    // "Sonam Bharti",
-    // "Rakesh Srikar Reddy",
-  ];
-  const roles = [
-    "Coordinator (Stage Management)",
-    "Coordinator (Stage Management)",
-    "Co-coordinator (Stage Management)",
-    "Coordinator (Event Management)",
-    "Member-1(Event Management)",
-    "Member-2(Stage Management)",
-    "Member-2(Event Management)",
-    "Member-1(Stage Management)",
-    "Coordinator (Event Management)",
-    "Coordinator (Stage Management)",
-    "Co-coordinator (Stage Management)",
-    "Coordinator (Event Management)",
-    "Co-coordinator (Event Management)",
-    "Co-coordinator (Event Management)",
-  ];
-=======
 	const images = [
 		image52,
 		image51,
@@ -609,99 +400,9 @@ function Management() {
 		"Co-coordinator (Event Management)",
 		"Co-coordinator (Event Management)",
 	]
->>>>>>> origin/charity
 
 	return <TeamSection images={images} names={names} roles={roles} />
 }
-<<<<<<< HEAD
-function Others() {
-  const images = [
-    image71,
-    image72,
-    image73,
-    image74,
-    image75,
-    image0,
-    image0,
-    image76,
-    image77,
-    image78,
-    image79,
-    image80,
-    image81,
-    image0,
-    image0,
-    image82,
-    image83,
-    image84,
-    image0,
-    image85,
-    image0,
-    image0,
-    image0,
-    image86,
-    image0,
-
-
-
-
-  ];
-  
-  const names = [
-    "Laxman",
-    "Vedant",
-    "Satyajeet",
-    "Vattimilli Divya Sree",
-    "Gowthaboina Trisha",
-   "Rakesh Bingi", // Image0 photo not found
-    "Refi Vasti islari", // Image0 photo not found
-    "Sravani",
-    "sanjana thothu",
-    "Rai Tripura",
-    "Rounak Saha",
-    "Arunoday Tiwari",
-    "Hemchand",
-    "Simma Kiran",// Image0 photo not found
-    "Dharmpal",// Image0 photo not found
-    "Rama Krishna",
-    "MD SHAMS TABREZ ANSARI",
-    "Dharam Singh",
-    "Rupesh",// Image0 photo not found
-    "Prinayshu Singh",
-    "Raj Sharma",// Image0 photo not found
-    "Gabriel",// Image0 photo not found
-    "Yaswanth",// Image0 photo not found
-    "Anil Kumar",
-    "Karan Gupta"// Image0 photo not found
-  ];
-  const roles = [
-    "Talk Show Coordinators (Task Force Club)",
-    "Talk Show Coordinators (Task Force Club)",
-    "Talk Show Co-Coordinators (Task Force Club)",
-    "Talk Show Members (Task Force Club)",
-    "Talk Show Members(Task Force Club)",
-    "Coordinator (Traditional Dance)",
-    "Coordinator (Traditional Dance)",
-    "Co-Coordinator (Traditional Dance)",
-    "Member (Traditional Dance)",
-    "Astronomy Coordinator ",
-    "Astronomy Member",
-    "Astronomy Member",
-    "Head (Volunteer)",
-    "Head (Volunteer)",
-    "Coordinator (Volunteer)",
-    "Coordinator (Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-    "Member (Robotic club Volunteer)",
-  ];
-=======
->>>>>>> origin/charity
 
 function Robotics() {
 	const images = [
@@ -826,12 +527,12 @@ function Coding() {
 		"Siddhant Purkayastha",
 		"Karipireddy Surya Teja",
 		"Ritabrata Pal",
-		"Guarav Joshi",
+		"Gaurav Joshi",
 		"Rounak Saha"
 	  ];
 	  
 	const roles = [
-		"Coordinator",
+		"Convener",
 		"Coordinator",
 		"Coordinator",
 		"Coordinator",
@@ -1133,38 +834,6 @@ const Team = () => {
 }
 
 function App() {
-<<<<<<< HEAD
-
-
-
-  return (
-    
-      
- 
-    <div className="overflow-clip custom-cursor"  >
-
-      <div className="h-full w-full bg-[#101010]">
-        <NavBar />
-        <div className="relative w-full">
-          <div className="absolute w-48 md:w-80 md:h-80 h-60 bg-purple-600 z-10 top-0 -left-10 rounded-full opacity-30 filter blur-xl animate-blob"></div>
-          <div className="absolute  w-48 md:w-80 md:h-80 h-48 delay-1000 bg-indigo-600 z-10 -top-20 -right-28 rounded-full opacity-30 filter blur-xl animate-blob"></div>
-          <div className="relative z-10">
-            <div className="text-center mx-auto z-50 w-full text-white pb-8">
-              {" "}
-              <div
-                style={{ letterSpacing: "-1px" }}
-                className="pt-10 mt-10 z-50 sm:pt-10 md:pt-10 text-4xl lg:text-4xl xl:text-5xl font-bold text-[#E8E8E8] text-center font-poppins tracking-wide leading-tight"
-              >
-                Meet the <span className="text-[#61E2F4]">team</span>
-              </div>
-              <div className="flex mb-10  flex-col justify-center flex-shrink-0 text-[#EBEBEB] opacity-75 font-plus-jakarta-sans text-sm md:text-lg md:mb-4 mt-2 px-12">
-                A special thanks to all the wonderful people who contributed to
-                the event! Words can&apos;t express our appreciation for you!
-              </div>
-            </div>
-          </div>
-        </div>
-=======
 	return (
 		<div className="overflow-clip">
 			<div className="h-full w-full bg-[#101010]">
@@ -1188,7 +857,6 @@ function App() {
 						</div>
 					</div>
 				</div>
->>>>>>> origin/charity
 
 				<div>
 					<Team />
